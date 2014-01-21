709827d60044ae2537c380f93a99db1d97aea592
Victor Olsson <kontakt@vicolsson.se>
2014-01-21 14:59:16 +0100

    Filip-fix

--- Webpage/site.js
+++ Webpage/site.js
@@ -1,7 +1,7 @@
 ﻿$( document ).ready(function() {	
 	var superadmin = false;
 	var producer = false;
-	var shopOwner = false;
+	var shopOwner = true;
 	var newData;
 	var fadeOutTime = 200;
 	var fadeInTime = 200;
