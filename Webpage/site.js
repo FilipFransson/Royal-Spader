f5bd9a8c2c859dbff2fa6b2aba23933410d71720
Victor Olsson <kontakt@vicolsson.se>
2014-01-21 08:43:37 +0100

    JS

---
+++ Webpage/site.js
@@ -0,0 +1,13 @@
+﻿$( document ).ready(function() {
+	function getMenu(){
+		$(".headermenu").html("Länk 1&nbsp;&nbsp;|&nbsp;&nbsp;Länk 2&nbsp;&nbsp;|&nbsp;&nbsp;Länk 3");
+	}
+	
+	function openPage(){
+		$(".maincontent").html("");
+		$(".maincontent").append("Välkommen!");
+	}
+	
+	openPage();
+	getMenu();
+});
